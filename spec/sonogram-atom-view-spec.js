b977f30621373a6974267f103d9c538cf9b5c1f6
TMiguelT <ttmigueltt@gmail.com>
2017-03-05 02:06:44 +1100

    Initial commit

---
+++ spec/sonogram-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SonogramAtomView from '../lib/sonogram-atom-view';
+
+describe('SonogramAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
